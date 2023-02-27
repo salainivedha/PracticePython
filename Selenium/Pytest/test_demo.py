9caf2c64bebf8f467e54e8e9abd03f9727a4131d
salainivedha <salainivedha@gmail.com>
2023-02-27 23:29:49 +0800

    Changes

---
+++ Selenium/Pytest/test_demo.py
@@ -0,0 +1,16 @@
+#Any pytest file should start with test_ or end with _test
+# Pytest method names should start with test
+# Any code should be wrapped in method only
+# Method name should have sense
+# -k stands for method name execution
+# -s logs in o/p
+#-v for more info in metadata
+# you can run specific file with py.test<filename>
+
+
+
+def test_firstProgram():
+    print("hello")
+
+
+
